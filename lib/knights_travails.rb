fd2f27bcf98bbc4b624440d76c0df0297c79ae85
Byohazyrd <benjaminmikkelson@gmail.com>
2021-03-16 11:14:28 -0600

    BFS working, but need to make it track the path

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -1,32 +1,16 @@
-#so what's the full breakdown here?
-
-#we have several components we're working with:
-  #a game board -- an array of arrays correlating to coordinates on a game board
-  #a "knight", which is essentially just a starting coordinate in the grid?
-  #we need a way to keep track of legal moves the knight can make. In other words, 
-  #from any given space, we need to keep track of all other spaces that can be moved to.
-
-#in practical terms, this means that a knight will have its own specific tree of legal moves based on its movement algorithm.
-#so for a knight, that's a directed cyclic graph with some nodes having a max of 8 child nodes. Adjacency matrix maybe?
-#those moves won't ever change, the only thing that changes is the knight's current coordinates, which can be displayed on the saved grid.
-
-
-#for the game board, we need an array of arrays. 
-
-#do we need a knight class? Not really. What the knight is is just a record of possible related moves using the right algorithm, right?
 
 class Cell
 
-  attr_accessor :position, :data, :moves
+  attr_accessor :position, :data, :moves, :visited
 
   def initialize(data = 0)
     @position = []
     @data = data
     @moves = []
+    @visited = 0
   end
 
 end
-
 
 class Board
 
@@ -65,8 +49,7 @@ class Board
     end
   end
 
-  #This is the method that has the juice. Takes a root node and recursively finds all the other nodes around it that would 
-  #count as a legal move. 
+  #Takes a root node and recursively finds all the other nodes around it that would count as a legal move.
   def find_knight_moves(root)
     
     #base case is if the root doesn't exist or if it's already been covered.
@@ -75,8 +58,6 @@ class Board
 
     #create local variables for the position's coordinates.
     y, x = root.position[0], root.position[1]
-    puts "y is #{y}"
-    puts "x is #{x}"
 
     #create local variables to store working array values
     possible_moves = []
@@ -92,8 +73,6 @@ class Board
     possible_moves << [(y - 1), (x - 2)]
     possible_moves << [(y - 1), (x + 2)]
 
-    puts "possible_moves are #{possible_moves}"
-
     #determines if each possible node is a valid move, and saves it if it is
     possible_moves.each do |move| 
       if (move[0] < 0 || move[1] < 0) || (move[0] > 7 || move[1] > 7)
@@ -103,14 +82,11 @@ class Board
       end
     end
 
-    puts "valid_moves is #{valid_moves}"
-
     #now that we've found all the valid moves for a node, marks the data flag showing we've covered the root node
     root.data = 1
 
     #Next, recursively finds the valid moves of each of the root node's valid children.
     valid_moves.each do |move|
-      puts "Found one! Adding #{move} to legal moves of #{root.position}"
       root.moves << move
       find_knight_moves(@grid[move[0]][move[1]])
     end
@@ -118,34 +94,60 @@ class Board
     return root
   end
 
+  def set_not_visited
+    @grid.each do |row|
+      row.each do |cell|
+        cell.visited == 0 if cell.visited != 0
+      end
+    end
+  end
+
+  def breadth_first_traversal(start_cell, target_cell)
+
+    root = @grid[start_cell[0]][start_cell[1]]
+    puts "Starting at #{root.position}"
+    puts "Looking for #{target_cell}"
+
+    #mark every cell as "not visited"
+    set_not_visited()
+
+    #create a queue for BFS
+    queue = []
+
+    #mark the root node as "visited" and enqueue it
+    root.visited = 1
+    queue << root.position
+
+    path = []
+
+    while queue.empty? == false do
+      node = queue.shift
+      path << node
+      if @grid[node[0]][node[1]] == @grid[target_cell[0]][target_cell[1]]
+        puts "Made it! Ending search."
+        break
+      else
+        @grid[node[0]][node[1]].moves.each do |move|
+          cell = @grid[move[0]][move[1]]
+          if cell.visited == 0
+            queue << cell.position
+            cell.visited = 1
+          end
+        end
+      end
+    end
+
+    puts "Your path is"
+    p path
+
+
+
+  end
+
+
+
+
 end
-
-# class Knight
-
-#   def initialize
-#     @possible_moves = find_knight_moves([0, 0])
-#   end
-
-#   def build_possible_moves(root = [0, 0])
-
-#     #here we need to pass in the game board, and build a tree off of it to determine all possible moves of each space in the entire grid
-#     #the tree will start with a root cell and find all legal moves from that space, and then save them as child nodes for that space.
-#     #so we should be able to recursively take a node and find it's children.
-#     #that should look something like:
-
-#     #base case is if the passed node doesn't exist or if it isn't a legal move
-
-#     return root if root == nil
-
-#     #we only continue the recursion if a node is determined tgame_board.knight_moves = find_knight_moves(@grid[0][0]) be a legal move. if it isn't, we return nothing or nil or something.
-#     if legal_move?(node)
-#       root.moves << node
-#       #---------- PAUSED METHOD HERE --------------
-
-#     end
-#   end
-
-#   #let's start with a more granular approach. Let's just take a node, and recursively find its available moves
 
 
 
@@ -158,6 +160,8 @@ game_board = Board.new
 
 
 game_board.display_board
+
+game_board.breadth_first_traversal([0, 0], [3, 3])
 
 
 
